5572dd77dc18e8cba66a4c003019ff9053720832
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-07 19:04:11 +0530

    merged remote and local repo

---
+++ src/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+    return (
+        <>
+            <main>
+                <h1>{"Reasons I'm learning React"}</h1>
+                <ol>
+                    <li>{"Can't think of much, but it's popular ig."}</li>
+                    <li>{"Not knowing can get one hell of a mockery out of me in this economy."}</li>
+                    <li>Unfortunately gotta get a j*b</li>
+                </ol>
+            </main>
+        </>
+    )
+}
+
+export default MainContent;
